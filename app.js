22ed1b65a97c9d598a39e9cbbee0109035b7ba9a
Ehsan <E55268199Yk>
2021-12-27 10:26:58 +0330

    deploying initail

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const fs = require("fs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
 const multer =require("multer");
 const { v4: uuidv4 } = require('uuid');
+const helmet = require("helmet");
+//**Most servers use compression and we don't have to use this middleware*/
+const compression = require("compression");
+const morgan =  require("morgan");
+
+const app = express();
 
 const feedRoutes = require("./routes/feed");
 const authRouts = require("./routes/auth");
 
-const app = express();
+
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -37,6 +44,10 @@ const fileFilter = (req, file, cb) => {
     }
 }
 
+//**For have logs in a file:*/
+//**{flags: "a"} means that it will add in the end of file*/
+const accessLogStream = fs.createWriteStream(path.join(__dirname,"access.log"),{flags: "a"})
+
 //**This is good for application/json files but the previous one was good for*/
 //**    x-www-form-urlencoded which we took from <form> of html.*/
 app.use(bodyParser.json());
@@ -58,6 +69,10 @@ app.use((req, res, next) => {
 //**This means that any request that start with "feed" will go to feedRoutes.*/
 app.use("/feed", feedRoutes);
 app.use("/auth", authRouts);
+app.use(helmet());
+//**Most servers use compression and we don't have to use this middleware*/
+// app.use(compression());
+app.use(morgan("combined",{stream: accessLogStream}))
 
 app.use((error,req, res, next) => {
     console.log("ERROR:::", error);
@@ -69,13 +84,12 @@ app.use((error,req, res, next) => {
 })
 
 //**Establish a mongoose connection*/
-mongoose.connect("mongodb+srv://ehsanScript:E55268199Yk@cluster0.ytldu.mongodb.net/messages?retryWrites=true&w=majority")
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ytldu.mongodb.net/messages?retryWrites=true&w=majority`)
     .then(result => {
         //**Also customized for socket io*/
-        const server = app.listen(8080);
-        const io = require("socket.io")(server);
+        const server = app.listen(process.env.PORT || 3000);
+        const io = require("./socket").init(server);
         io.on("connection", socket => {
-            console.log("CLIENT CONNECTED")
             console.log("CLIENT CONNECTED")
         })
     })
